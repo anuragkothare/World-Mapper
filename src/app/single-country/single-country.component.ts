4d435e5a2de3f519f3b69a510ede30442ae8ffb9
anuragkothare <anuragkothare@outlook.com>
2018-08-09 12:49:07 +0530

    Fixed Single Country Component

--- src/app/single-country/single-country.component.ts
+++ src/app/single-country/single-country.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute, Router } from "@angular/router";
 export class SingleCountryComponent implements OnInit {
 
   public currentCountry;
-  public allCountries
+  public allCountries$
 
   constructor(private _route: ActivatedRoute, private router: Router, public _http: CountryService) {
     console.log("Single Country constructor called.")
@@ -34,36 +34,23 @@ export class SingleCountryComponent implements OnInit {
       }
     )
 
-    // get countries of same currency
-    let currency_code = this._route.snapshot.paramMap.get('currencyCode');
-    console.log(currency_code);
-
-    this._http.getAllCountriesByCurrency(currency_code).subscribe(
-      data => {
-        console.log(data);
-        this.allCountries = data;
-      },
-      error => {
-        console.log("some error occured");
-        console.log(error.errorMessage)
+    // get countries of same region
+    this._route.params.subscribe(param => {
+      if(param['regionName']){
+        console.log("region called " + param['regionName'] )
+        this.allCountries$ = this._http.getAllCountriesByRegion(param['regionName'])
       }
-    )
-
-    // get countries of same language
-    let language_code = this._route.snapshot.paramMap.get('languageCode');
-    console.log(language_code);
-
-    this._http.getAllCountriesByLanguage(language_code).subscribe(
-
-      data => {
-        console.log(data);
-        this.allCountries = data;
-      },
-      error => {
-        console.log("some error occured");
-        console.log(error.errorMessage)
+      // get countries of same currency
+      else if(param['currencyCode']){
+        console.log("currenct called " + param['currencyCode'] )
+        this.allCountries$ = this._http.getAllCountriesByCurrency(param['currencyCode'])
       }
-    )
+      // get countries of same language
+      else if(param['languageCode']){
+        console.log("language called " + param['languageCode'] )
+        this.allCountries$ = this._http.getAllCountriesByLanguage(param['languageCode'])
+      }
+  })
 
   }
 }
